da34694cfab3fcd3bc85ccba6ad0444b76790dc7
abdelrhman ashraf <mido59478@gmail.com>
2023-02-25 21:59:00 +0200

    before end work

--- weth.js
+++ weth.js
@@ -7,18 +7,29 @@ const selectBoxLang = document.getElementById("lang-select");
 const input = document.getElementById("search");
 const searchBtn = document.getElementById("btn-search");
 
+const cardMap = document.querySelector(".card_weather .part_map");
+
+let tempBtns = '';
+
 
 // option variables
 const apiKey = "c5b938c99e40b31dd1f5ae5aad327606";
-let apiLang = 'en', valueInput = '';
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+let apiLang = 'en', valueInput = '', tempC = 'f';
 
+// All Changes To Show Weather Result On Page
+// When Click On search button
 searchBtn.addEventListener("click", ()=> {
 	valueInput = input.value.toLowerCase();
 	console.log(valueInput);
 	show(getapi(valueInput, apiLang))
 });
 
+// When Click On [ Enter ] in Keyboard From Input Search
 input.addEventListener("keyup", (event)=> {
     if(event.key == "Enter" || event.key == "enter") {
 		valueInput = input.value.toLowerCase();
@@ -27,18 +38,17 @@ input.addEventListener("keyup", (event)=> {
     }
 })
 
+// When Change The Language From Select Box
 selectBoxLang.onchange = () => {
 	apiLang = selectBoxLang.value;
 	if(valueInput !== ''){
-		const res = getapi(valueInput, apiLang).then(data =>  {
-			return data
-		}).catch(err => console.log(err));
+		show(getapi(valueInput, apiLang));
 	}
 }
 
 
 
-
+// To Open And Close Modal
 pMoreBtn.onclick = ()=> {
 	let ModalDiv = document.querySelector('.'+pMoreBtn.dataset.class);
 	ModalDiv.classList.add("show");
@@ -48,6 +58,7 @@ pMoreBtn.onclick = ()=> {
 		}
 	})
 }
+
 // btn.onclick = () => {
 //     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=${apiKey}`)
 // 	.then(response => response.json())
@@ -88,8 +99,6 @@ pMoreBtn.onclick = ()=> {
 // 	});
 // }
 
-/* Function Multiple fetches data from ( openweather ) website v1.2
-*/
 
 
 // Defining async function
@@ -107,31 +116,121 @@ async function getapi(value, lang = 'en') {
     return data;
 }
 
-// Function to define innerHTML for HTML table
+// Function to define innerHTML for HTML Result Container
 function show(data) {
-//     let tab =
-//         `<tr>
-//           <th>Name</th>
-//           <th>Office</th>
-//           <th>Position</th>
-//           <th>Salary</th>
-//          </tr>`;
-
-//     // Loop to access all rows
-//     for (let r of data.list) {
-//         tab += `<tr>
-//     <td>${r.name} </td>
-//     <td>${r.office}</td>
-//     <td>${r.position}</td>
-//     <td>${r.salary}</td>
-// </tr>`;
-//     }
-//     // Setting innerHTML as tab variable
-//     document.getElementById("employees").innerHTML = tab;
+	let content;
 	data.then(res =>  {
-		console.log(res);
-		return res;
-	}).catch(err => console.log(err));
+		console.log(res[1]);
+		let fore = '';
+
+		for(let i = 0; i < res[1].list.length; i++){
+			fore+= `<div class="fore">
+						<div class="row icon_num_fore">
+							<div class="icon-fore col-md-4 col-4 col-sm-12">
+								<img src="https://openweathermap.org/img/wn/${res[1].list[i].weather[0].icon}@2x.png" />
+							</div>
+							<div class="num-fore col-md-8 col-8 col-sm-12">
+								<p>${getTemp(res[1].list[i].main.temp, tempC)}°</p>
+							</div>
+						</div>
+						<div class="date-fore">
+							<span>Sun, Feb26</span>
+							<span>${res[1].list[i].weather[0].main}, ${res[1].list[i].weather[0].description}</span>
+						</div>
+						<div class="row collection-info-fore">
+							<div class="col-6 col-md-6">
+								<p>Feels Like <span>${getTemp(res[1].list[i].main.feels_like, tempC)}</span>°</p>
+								<p>Sea Level <span class="material-symbols-outlined">waves</span><span>${res[1].list[i].main.sea_level}</span> in</p>
+								<p>Humidity <span>${res[1].list[i].main.humidity}</span>%</p>
+							</div>
+							<div class="col-6 col-md-6">
+								<p>Wind <span class="material-symbols-outlined">air</span><span>${res[1].list[i].wind.speed}</span>m/s N</p>
+								<p>Visibility <span>${res[1].list[i].visibility.toLocaleString().substr(0, 4).replace(",", ".")}}</span>km</p>
+								<p>Pressure <span>${res[1].list[i].main.pressure}</span>°</p>
+							</div>
+						</div>
+					</div>`;
+		}
+
+		content = `<div class="row big_info_content">
+						<div class="name-deg">
+							<span>Feb 25, 06:18am</span>
+							<h1>${res[0].name}, ${res[0].sys.country}</h1>
+							<div class="content-info">
+								<span>SUNRISE: 06:27am <span class="material-symbols-outlined">sunny_snowing</span></span>
+								<span>SUNSET: 05:49pm <span class="material-symbols-outlined">wb_twilight</span></span>
+							</div>
+						</div>
+						<div class="icon-deg col-md-4 col-4 col-sm-12">
+							<img src="https://openweathermap.org/img/wn/${res[0].weather[0].icon}@2x.png" />
+						</div>
+						<div class="num-deg col-md-8 col-8 col-sm-12">
+							<p>${getTemp(res[0].main.temp, tempC)}</p>
+							<div class="ch_deg">
+								<span class="tempBtn" data-temp="f">°F</span>
+								<hr>
+								<span class="tempBtn" data-temp="c">°C</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="row text-stat">
+						<h3>${res[0].weather[0].main}, ${res[0].weather[0].description}</h3>
+					</div>
+
+					<div class="row collection-info">
+						<div class="col-6 col-md-6">
+							<p>Feels Like <span>${getTemp(res[0].main.feels_like, tempC)}</span>°</p>
+							<p>Humidity <span>${res[0].main.humidity}</span>%</p>
+						</div>
+						<div class="col-6 col-md-6">
+							<p>Wind <span class="material-symbols-outlined">air</span><span>${res[0].wind.speed}</span>m/s NNE</p>
+							<p>Visibility <span>${res[0].visibility.toLocaleString().substr(0, 4).replace(",", ".")}</span>km</p>
+						</div>
+					</div>
+
+					<hr>
+
+					<div class="row hourly_forecast">
+						<p class="fore-title">Hourly Forecast</p>
+						<div class="scroll_content_forecs">
+							${fore}
+						</div>
+					</div>`;
+
+		document.querySelector(".card_weather .part_info .container_result_info").innerHTML = content;
+
+		const tempBtns = document.querySelectorAll(".num-deg .ch_deg .tempBtn");
+		tempBtns.forEach(btn => {
+			if(btn.dataset.temp === tempC){
+				btn.classList.add("active");
+			}
+
+			btn.addEventListener("click", ()=> {
+				if(btn.dataset.temp === 'c') {
+					tempC = 'c';
+				} else if(btn.dataset.temp === 'f'){
+					tempC = 'f';
+				}
+				show(getapi(valueInput, apiLang));
+			})
+		})
+
+		cardMap.classList.add("show");
+	}).catch(err => {
+		content = `<div class="empty_result">
+						<div class="empty-img">
+							<img src="https://static.thenounproject.com/png/4147389-200.png" alt="Weather Empty" />
+						</div>
+						<div class="empty-info">
+							<p class="text-center">There's Is A Problem, or Not Found Country, City</p>
+						</div>
+					</div>`;
+
+		console.log(err);
+		cardMap.classList.remove("show");
+		document.querySelector(".card_weather .part_info .container_result_info").innerHTML = content;
+	});
 }
 
 
@@ -142,13 +241,13 @@ function show(data) {
 ** degF = accept string value { 'c' | 'f' } else will not work the function
 ** Real Example >>> console.log(Ktoc(290.05, 'f')) >> 61 °F
 */
-async function Ktoc(K, degF = 'c'){
-	if(degF === 'c') {
+function getTemp(K, state = 'f'){
+	if(state === 'c') {
 		return Math.floor(K - 273.15);
-	} else if(degF === 'f'){
+	} else if(state === 'f'){
 		let celsiusTemp = Math.floor(K - 273.15);
-		return  Math.ceil((celsiusTemp * 1.8) + 32);
-	} else { return; }
+		return Math.ceil((celsiusTemp * 1.8) + 32);
+	} else{ return; }
 }
 
 
@@ -157,45 +256,8 @@ async function Ktoc(K, degF = 'c'){
 
 const tabsBox = document.querySelector(".hourly_forecast .scroll_content_forecs"),
 allTabs = document.querySelectorAll(".hourly_forecast .scroll_content_forecs .fore");
-// arrowIcons = document.querySelectorAll("header .icon span");
 
 let isDragging = false;
-
-// const handleIcons = ()=> {
-//     let scrollVal = Math.round(tabsBox.scrollLeft);
-//     let maxScrollableWidth = tabsBox.scrollWidth - tabsBox.clientWidth;
-//     arrowIcons[0].parentElement.style.display = scrollVal > 0 ? 'flex': 'none';
-//     arrowIcons[1].parentElement.style.display = maxScrollableWidth > scrollVal ? 'flex': 'none';
-// }
-
-// arrowIcons.forEach(icon => {
-//     icon.addEventListener('click', ()=> {
-//         // if clicked Icon is left, reduce 350 from tabsBox scrollLeft else add
-//         tabsBox.scrollLeft += icon.id === 'left'? -350: 350;
-//         setTimeout(()=> handleIcons(), 50); // calling handleIcons after 50 milliseconds
-//     })
-// })
-
-// allTabs.forEach(tab => {
-//     tab.addEventListener('click', ()=> {
-//         // removing active class from the previous tab & adding to current clicked tab
-//         document.querySelector(".tabs_box .active").classList.remove("active");
-//         tab.classList.add("active");
-
-//         let eleShowClass = document.querySelector("#app .big_content.show");
-//         let eleTabClass = document.querySelector(`#app .big_content.${tab.dataset.class}`);
-//         if(eleShowClass !== null && eleTabClass !== null){
-//             eleShowClass.classList.remove("show");
-//         }
-//         if(eleTabClass !== null){
-//             setTimeout(()=> eleTabClass.classList.add("show"), 500);
-//         }
-
-//         // change the Page Title With Another Form
-//         if(tab.dataset.class == 'games') document.title = 'Join Our( W.D )World';
-//         if(tab.dataset.class == 'bootst') document.title = 'World Faster With Bootstrap';
-//     })
-// })
 
 const dragging = (e)=> {
     if(!isDragging)return;
